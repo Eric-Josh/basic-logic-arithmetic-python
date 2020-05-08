7799af0d9e1bfe320e9080456580396d00ec0f5f
Joshua Ezeh <techjosmar@gmail.com>
2020-05-08 11:04:07 +0100

    Add existing file

--- leap-year.py
+++ leap-year.py
@@ -9,4 +9,4 @@ getUserYear = int(input("Kindly enter a year from 2000 and above: "))
 for year in range(2000, getUserYear):
 	#check if the year is a Leap year if yes print
 	if (0 == year % 4) and (0 != year % 100) or (0 == year % 400):
-		print(year);
+		print(year)
